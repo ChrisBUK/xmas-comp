3313d60560273069c0b2659a8fefd6d138460bc3
Chris Booker <chrisbooker@Chriss-Mac-mini.local>
2015-12-14 21:21:44 +0000

    Further gains

--- xmas.py
+++ xmas.py
@@ -5,27 +5,26 @@ def dedupe(i): return len(set(str(i))) == 3
 def strip(p, i, l):
 	rl = []
 	for li in l:
-		if str(li)[p] == i:
+		if i == li[p]:
 			rl.append(li)
 	return rl
 def run():
 	jm = map(str, filter(dedupe, range(124,962,31)))
-	for a in filter(dedupe, range(120,980,30)):
-		l = list(str(a))
+	for a in map(str, filter(dedupe, range(120,980,30))):
+		l = list(a)
 		for m in strip(1,l[0], jm):
 			if not (l[0] != m[1] or m[0] in l or m[2] in l):
 				l.extend([m[0], m[2]])
 				for j in strip(1,l[0], jm):
 					if not (l[0] != j[1] or j[0] in l or j[2] in l):
 						l.extend([j[0], j[2]])
-						for f in range(140,980,28):
-							sf = str(f)
-							if not (sf[0] in l or sf[1] in l or sf[2] in l):
-								return [sf[0], sf[1], sf[2]]
+						for f in map(str,range(140,980,28)):
+							if not (f[0] in l or f[1] in l or f[2] in l):
+								return '%s%s%s' % (f[0], f[1], f[2])
 
 if __name__ == '__main__':
 	t = time.time()
-	a = ''.join(run())
+	a = run()
 	e = time.time() - t
 
 	sys.stdout.write(str('%s\n%iµs\n' % (a, e * 1000000)))
--- xmas_multi_iterator.py
+++ xmas_multi_iterator.py
@@ -6,23 +6,22 @@ def dedupe(i): return len(set(str(i))) == 3
 def strip(p, i, l):
 	rl = []
 	for li in l:
-		if i == str(li)[p]:
+		if i == li[p]:
 			rl.append(li)
 	return rl
 def run():
 	jm = map(str, filter(dedupe, range(124,962,31)))
-	for a in filter(dedupe, range(120,980,30)):
-		l = list(str(a))
+	for a in map(str, filter(dedupe, range(120,980,30))):
+		l = list(a)
 		for m in strip(1,l[0], jm):
 			if not (l[0] != m[1] or m[0] in l or m[2] in l):
 				l.extend([m[0], m[2]])
 				for j in strip(1,l[0], jm):
 					if not (l[0] != j[1] or j[0] in l or j[2] in l):
 						l.extend([j[0], j[2]])
-						for f in range(140,980,28):
-							sf = str(f)
-							if not (sf[0] in l or sf[1] in l or sf[2] in l):
-								return [sf[0], sf[1], sf[2]]
+						for f in map(str,range(140,980,28)):
+							if not (f[0] in l or f[1] in l or f[2] in l):
+								return [f[0], f[1], f[2]]
 
 if __name__ == '__main__':
 	it = 1000
